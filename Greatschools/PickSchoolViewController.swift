de1c0e8f51f00c3d10e92caffc93445fdb07df85
sunnyqin <sunny@interactivelabs.co>
2016-03-08 10:29:37 +0800

    update display name/ update app icons

---
+++ Greatschools/PickSchoolViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PickSchoolViewController.swift
+//  Greatschools
+//
+//  Created by SunyQin on 3/8/16.
+//  Copyright © 2016 ilabs. All rights reserved.
+//
+
+import UIKit
+
+class PickSchoolViewController: UIViewController {
+	
+}
+
